e35d82c8e961e73a49e10f2e1572461af8a6cf62
VenkataSai <saisiva249@gmail.com>
2019-12-17 14:35:02 +0530

    Update Questions.py

--- Questions.py
+++ Questions.py
@@ -1,2 +1,4 @@
 https://www.programiz.com/python-programming/examples
 https://github.com/zhiwehu/Python-programming-exercises/blob/master/100%2B%20Python%20challenging%20programming%20exercises.txt
+https://data-flair.training/blogs/python-projects-with-source-code/
+  
