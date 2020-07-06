71aa0b0ccd48e7fd99bd792d8aa7e3a105ab5d23
VenkataSai <saisiva249@gmail.com>
2020-07-06 21:40:58 +0530

    Patterns
    Pattern1 2

---
+++ Patterns/Pattern1.py
@@ -0,0 +1,13 @@
+'''
+*
+**
+***
+****
+*****
+'''
+
+# printing pattern based on matrix column value i,j
+# i=row j = column
+num = int(input("Enter number of rows"))
+for i in range(1,num+1):
+    print("*"*i)
---
+++ Patterns/Pattern2.py
@@ -0,0 +1,9 @@
+'''
+   *
+  ***
+ *****
+*******
+'''
+num = int(input("enter number of lines"))
+for i in range(1,num+1):
+    print(" "*(num-i)+"*"*(2*i-1))
