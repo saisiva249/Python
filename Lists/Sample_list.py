56116bb7643ebe45338fe8d9ba1b36f551eb5a63
VenkataSai <saisiva249@gmail.com>
2020-07-07 09:20:21 +0530

    LIsts in python
    Different types and differences and list theory

---
+++ Lists/Sample_list.py
@@ -0,0 +1,8 @@
+# Lists are denoted using[]
+# they are ordered and mutable also allows duplicate data.
+# as they are ordered indexing(positive or negative) can be applied for getting elements out of it
+# Range of indexes can be applied list[2:5], or list[-4,-1]
+# we can change item value as it is immutable using index list[3] = "new_value" 
+# we can get the length of a list using len(list)
+# Add item we can add a item to list using append list.append()
+# removing an item from list can be done using list.remove() or pop() or clear() or del
