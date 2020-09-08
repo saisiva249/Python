39c36ddb211051612c76b05709e86cc932a2f9c0
venkata sai siva kumar g <saisiva249@gmail.com>
2020-09-08 21:57:54 +0530

    modfied the yield function

---
+++ yieldExample.py
@@ -0,0 +1,19 @@
+def cube(n):
+    return n**3
+
+def simplifiedCubeGenerator(n):
+    generatedResults =[]
+    currentN = 1
+    while currentN<=n:
+        generatedResults.append(cube(currentN))
+        currentN+=1
+    return generatedResults
+
+def generateUsingYield(n):
+    currentN = 1
+    while currentN <=n:
+        yield cube(currentN)
+        currentN+=1
+
+print(simplifiedCubeGenerator(5))
+print([i for i in generateUsingYield(5)])
